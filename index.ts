a60b47029ad1858bd583e6a957be4eb65a03d570
Hamo Muradyan <hamlet@advancedtech.am>
2024-02-20 18:08:21 +0400

    declarations

--- index.ts
+++ index.ts
@@ -1,24 +1,32 @@
-import axios from "axios";
+let test0 = "Ester";
+console.log(test0);
 
-const url = "https://jsonplaceholder.typicode.com/todos/1";
+let test1: boolean;
+test1 = true;
+console.log(test1);
 
-interface Todo {
-  id: number;
-  title: string;
-  completed: boolean;
-}
+let yourName: string;
+yourName = "Valod";
+console.log(yourName);
 
-axios.get(url).then((response) => {
-  const todo = response.data as Todo;
-  const id = todo.id;
-  const title = todo.title;
-  const finished = todo.completed;
+let test2: string | number; //union
+test2 = 465;
+test2 = "test2";
+console.log(test2);
 
-  logTodo(id, title, finished);
-});
+type Name = string;
+const myName: Name = "Hamlet";
+console.log(myName);
 
-function logTodo(id: number, title: string, completed: boolean) {
-  console.log(
-    `ToDo id: ${id}, ToDo Title: ${title} Todo Completed: ${completed}`
-  );
-}
+type Score = number | string; // Declaring the type - հայտարարում ենք տիպը
+const myScore: Score = 8; // it calls "allias" - սա կոչվում է ալիաս
+console.log(myScore);
+
+let x1: number = JSON.parse("6"); // this function returned ANY and we should check value
+console.log(x1);
+let x2: string = JSON.parse("9");
+console.log(x2);
+
+let test3:any;
+test3 = 'e';
+console.log(test3);
